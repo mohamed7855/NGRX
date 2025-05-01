f3c1497809f73d7d1dc9d8f7233e4b1540cfb074
mohamed7855 <martm7726@gmail.com>
2025-05-01 03:47:41 +0300

    select data in component to display

--- src/app/components/counter-output/counter-output.component.ts
+++ src/app/components/counter-output/counter-output.component.ts
@@ -1,28 +1,18 @@
 import { Component } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { CounterService } from '../../services/counter.service';
+import { Observable, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-counter-output',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './counter-output.component.html',
   styleUrl: './counter-output.component.css',
 })
 export class CounterOutputComponent {
-  counter = 0;
-  counterServiceSub?: Subscription;
+  count$: Observable<number>;
 
-  constructor(private counterService: CounterService) {}
-
-  ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe(
-      (newValue) => (this.counter = newValue)
-    );
-  }
-
-  ngOnDestroy(): void {
-    if (this.counterServiceSub) {
-      this.counterServiceSub.unsubscribe();
-    }
+  constructor(private store: Store<{ counter: number }>) {
+    this.count$ = this.store.select('counter');
   }
 }
