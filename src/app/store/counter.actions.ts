47fcd2a8459c8525ceb009fcc45b5cd2f241c9b3
mohamed7855 <martm7726@gmail.com>
2025-05-01 16:55:03 +0300

    go to updated way again

--- src/app/store/counter.actions.ts
+++ src/app/store/counter.actions.ts
@@ -1,26 +1,26 @@
-import { Action } from '@ngrx/store';
+import { Action, createAction, props } from '@ngrx/store';
 
 export const INCREMENT = '[Counter] Increment';
 export const DECREMENT = '[Counter] decrement';
 
-// export const incerment = createAction(
-//   '[Counter] Increment',
-//   props<{ value: number }>()
-// );
+export const incerment = createAction(
+  INCREMENT,
+  props<{ value: number }>()
+);
 
-// export const decrement = createAction(
-//   '[Counter] decrement',
-//   props<{ value: number }>()
-// );
+export const decrement = createAction(
+  DECREMENT,
+  props<{ value: number }>()
+);
 
-export class IncrementAction implements Action {
-  readonly type: string = INCREMENT;
-  constructor(public value: number) {}
-}
+// export class IncrementAction implements Action {
+//   readonly type: string = INCREMENT;
+//   constructor(public value: number) {}
+// }
 
-export class DecrementAction implements Action {
-  readonly type: string = DECREMENT;
-  constructor(public value: number) {}
-}
+// export class DecrementAction implements Action {
+//   readonly type: string = DECREMENT;
+//   constructor(public value: number) {}
+// }
 
-export type CounterActions = IncrementAction | DecrementAction;
+// export type CounterActions = IncrementAction | DecrementAction;
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,30 +1,30 @@
-// import { createReducer, on } from '@ngrx/store';
-// import { decrement, incerment } from './counter.actions';
+import { createReducer, on } from '@ngrx/store';
+import { decrement, incerment } from './counter.actions';
 
-import { Action } from '@ngrx/store';
-import {
-  CounterActions,
-  DECREMENT,
-  DecrementAction,
-  INCREMENT,
-  IncrementAction,
-} from './counter.actions';
+// import { Action } from '@ngrx/store';
+// import {
+//   CounterActions,
+//   DECREMENT,
+//   DecrementAction,
+//   INCREMENT,
+//   IncrementAction,
+// } from './counter.actions';
 
 let initialState = 0;
-// export const counterReducer = createReducer(
-//   initialState,
+export const counterReducer = createReducer(
+  initialState,
 
-//   on(incerment, (state, action) => state + action.value),
+  on(incerment, (state, action) => state + action.value),
 
-//   on(decrement, (state, action) => state - action.value)
-// );
+  on(decrement, (state, action) => state - action.value)
+);
 
-export function counterReducer(state = initialState, actions: Action) {
-  if (actions.type == INCREMENT) {
-    return state + (actions as CounterActions).value;
-  } else if (actions.type == DECREMENT) {
-    return state - (actions as CounterActions).value;
-  }
+// export function counterReducer(state = initialState, actions: Action) {
+//   if (actions.type == INCREMENT) {
+//     return state + (actions as CounterActions).value;
+//   } else if (actions.type == DECREMENT) {
+//     return state - (actions as CounterActions).value;
+//   }
 
-  return state;
-}
+//   return state;
+// }
